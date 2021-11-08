262b1d420ec997ff271c45f6f6e6a10fdecfe8e7
jrrohrer <jessie.rohrer@gmail.com>
2021-11-08 11:04:06 -0500

    add content and styling to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+// import headerLogo from '../logo.png'
 
 const Header = () => {
   return (
     <div className='header-wrapper'>
       <div className='main-info'>
-        <h1>Jessie Rohrer</h1>
+        {/* <img src={headerLogo} alt="Jessie Rohrer: Software Engineer" /> */}
+        <h1>Software Engineer, Web Developer, Front End Developer</h1>
       </div>
     </div>
   )
