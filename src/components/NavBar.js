00ee88b2a20a0eefdb37d7a0f324442004673b85
jrrohrer <jessie.rohrer@gmail.com>
2021-11-08 10:06:11 -0500

    change hamburger menu icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import logo from '../logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import React from 'react'
 
@@ -9,7 +11,14 @@ const NavBar = () => {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
             <Navbar.Brand href="/"><img src={logo} alt="navbar logo"/></Navbar.Brand>
-            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+              <span>
+                <FontAwesomeIcon
+                  icon={faBars}
+                  style={{color: '#ac9c6c'}}
+                />
+              </span>
+            </Navbar.Toggle>
             <Navbar.Collapse id='responsive-navbar-nav'>
               <Nav className="ms-auto">
                 <Nav.Link href='#'>Home</Nav.Link>
