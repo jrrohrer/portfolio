275d0e7170a60b98ddccff1240597035686e7f4b
jrrohrer <jessie.rohrer@gmail.com>
2021-11-22 18:07:49 -0500

    fix positioning of components on home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer id='sticky-footer' className='flex-shrink-0 py-4 bg-dark text-white-50 fixed-bottom'>
+    <footer id='sticky-footer' className='flex-shrink-0 py-4 bg-dark text-white-50'>
       <div className='container text-center'>
         <span className='text-muted'>&copy; Jessie Rohrer, 2021</span>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -14,13 +14,13 @@ const Home = () => {
         </div>
       </div>
       <div className='content text-center pt-5'>
-        <h2>Thanks for stopping by</h2>
+        <h2>Thanks for stopping by!</h2>
         <p className='p-3'>I'm Jessie, and I'm a social worker turned software engineer. Learn more about me, my experience, and my portfolio here. Want to get connected? Reach out to me on one of these platforms:</p>
-        <div className='link-container'>
-          <a href='https://dev.to/jrrohrer'><img src={devtoLogo} alt='Click here to visit my blog on Dev.to' height='50px'/></a>
-          <a href='https://github.com/jrrohrer'><img src={githubLogo} alt='Click here to visit my github profile' height='50px' /></a>
-          <a href='https://www.linkedin.com/in/jessie-rohrer/'><img src={linkedinLogo} alt='Click here to visit my LinkedIn profile' height='50px' /></a>
-          <a href='https://twitter.com/jrrohrer'><img src={twitterLogo} alt='Click here to visit my Twitter' height='50px' /></a>
+        <div className='link-container pb-3'>
+          <a href='https://dev.to/jrrohrer'><img src={devtoLogo} alt='Click here to visit my blog on Dev.to' height='50px' className='m-2'/></a>
+          <a href='https://github.com/jrrohrer'><img src={githubLogo} alt='Click here to visit my github profile' height='50px' className='m-2' /></a>
+          <a href='https://www.linkedin.com/in/jessie-rohrer/'><img src={linkedinLogo} alt='Click here to visit my LinkedIn profile' height='50px' className='m-2' /></a>
+          <a href='https://twitter.com/jrrohrer'><img src={twitterLogo} alt='Click here to visit my Twitter' height='50px' className='m-2' /></a>
         </div>
       </div>
     </>
