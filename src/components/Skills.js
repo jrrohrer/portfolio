25bab0d14c99f8e3c2bf3523b554275b0ce0dea1
jrrohrer <jessie.rohrer@gmail.com>
2021-12-01 17:13:03 -0500

    remove vertical timeline package; add content to skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,8 +5,20 @@ const Skills = () => {
     <div id='skills' tabIndex='8'>
       <hr style={{ width: '50%', height: '5px', color: '#475841', margin: 'auto'}} className='mt-5 mb-5' />
 
-      <h1 className='text-center'>Skills</h1>
-
+      <h1 className='text-center pb-3'>Skills</h1>
+      <div className='container fluid text-center'>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt='HTML 5 icon'/>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt='CSS 3 icon' />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt='Bootstrap icon' />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original-wordmark.svg" alt='Ruby icon' />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg" alt='Ruby on Rails icon' />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='JavaScript icon' />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt='React icon' />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt='Redux icon' />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt='GitHub icon' />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" alt='VSCode icon' />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain-wordmark.svg" alt='Ubuntu icon' />
+      </div>
     </div>
   )
 }
