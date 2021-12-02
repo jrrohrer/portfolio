0d0f665b591d584d0d2d74a119c6e29ada90d64d
jrrohrer <jessie.rohrer@gmail.com>
2021-12-01 21:18:35 -0500

    add gh-pages package; compressed large images

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,10 @@ const About = () => {
             <Image src={profilePhoto} alt='Jessie Rohrer' fluid roundedCircle className='shadow w-50' />
           </Col>
           <Col>
-            <p className='m-3'>I am a software engineer with a background in social work. I'm a recent graduate of the Flatiron School software engineering program, and I have a prior BA in Psychology from Shippensburg University. My current tech stack is Ruby, Rails, HTML, CSS, JavaScript, React, & Redux. I have been building websites for fun since high school, and decided to persue web development as a career after leaving social work to raise my two young children, who are now in elementary school. I have spent the last year learning not just the languages needed to meet that goal, but also the tools and culture surrounding them, while at the same time guiding my kids through their own online learning. I am interested in web accessibility, and am now taking dives into a11y best practices, as well as learning data structures and algorithms.</p>
+            <p className='m-3'>
+              I'm a software engineer with a background in social work. I'm a recent graduate of the Flatiron School software engineering program, and I have a BA in Psychology from Shippensburg University. My current tech stack is Ruby, Rails, HTML, CSS, JavaScript, React, & Redux. I have been building websites for fun since high school, and decided to persue web development as a career after leaving social work to raise my two young children, who are now in elementary school. I have spent the last year learning not just the languages needed to meet that goal, but also the tools and culture surrounding them, while at the same time guiding my kids through their own online learning. I am interested in web accessibility, and am now taking dives into a11y best practices, as well as learning data structures and algorithms.<br/>
+              My non-tech interests include fiber arts (such as knitting and spinning yarn), casual hiking & biking, car camping with my family, playing trombone, and painting with watercolors.
+            </p>
           </Col>
         </Row>
       </Container>
